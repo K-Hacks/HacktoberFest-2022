814749574ac7b8ef85fdc60e5b178be031415bf7
Tara-1022 <taraann@karunya.edu.in>
2022-10-22 00:25:15 +0530

    Fix bugs in the GUI, add a small description

--- Game_of_Life/Game_GUI.py
+++ Game_of_Life/Game_GUI.py
@@ -8,7 +8,7 @@ x, y = 5, 5
 def start():
     for r in range(x):
         for c in range(y):
-            if cells[r][c] == 1:
+            if cells[r][c].get() == 1:
                 selected_cells.add(Cell(r, c))
     show_evolution(selected_cells)
 
@@ -20,23 +20,25 @@ def show_evolution(curr_cells):
     for r in range(x):
         for c in range(y):
             if Cell(r, c) in board.alive_cells:
-                b = Checkbutton(window, bg="black", onvalue=1, offvalue=0, indicatoron=False, width=3,
+                b = Checkbutton(window, bg="yellow", onvalue=1, offvalue=0, indicatoron=False, width=3,
                             variable=cells[r][c])
             else:
-                b = Checkbutton(window, bg="light gray", onvalue=1, offvalue=0, indicatoron=False, width=3,
+                b = Checkbutton(window, bg="black", onvalue=1, offvalue=0, indicatoron=False, width=3,
                                 variable=cells[r][c])
             b.grid(row=r + 1, column=c + 1)
-    Button(window, text = "Next", fg = "white", bg = "dark gray", command = lambda: show_evolution(board.evolve())).grid(row = x+1,column = y+1)
-    Button(window, text="Quit", fg="white", bg="dark gray", command=lambda: window.destroy()).grid(
+            b.deselect()
+    Button(window, text = "Next", fg = "gold", bg = "black", command = lambda: show_evolution(board.evolve())).grid(row = x+1,column = y+1)
+    Button(window, text="Quit", fg="white", bg="red", command=lambda: window.destroy()).grid(
         row=x + 2, column=y + 1)
 
 #initial set-up window
 window = Tk()
 window.title("Game of Life")
-window.geometry('400x400')
+window.geometry('400x200')
+window.resizable(False, False)
 cells = [[IntVar() for r in range(x)] for c in range(y)]
 buttons = [[None]*y]*x
-selected_cells = {}
+selected_cells = set()
 for r in range(x):
     for c in range(y):
         b = Checkbutton(window, bg = "dark gray", onvalue=1, offvalue=0, indicatoron = False, width = 3, variable = cells[r][c])
@@ -47,3 +49,4 @@ Label(window, text = "Select a cell to indicate it's alive.\n Click 'start' when
 start = Button(window, text = "Start", fg = "gold", bg = "black", command = start)
 start.grid(row = x+2, column = y + 1)
 
+window.mainloop()
--- Game_of_Life/tester.py
+++
@@ -1,6 +0,0 @@
-from Game_of_Life import *
-g1 = Grid(5, 5, {Cell(x, 2) for x in range(1, 4)})
-print(g1)
-for i in range(5):
-	g1 = Grid(5, 5, g1.evolve())
-	print(g1)
